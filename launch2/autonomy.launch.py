fc3ff1628a94c70d90f08a039ae02a711ed5b3c3
William L. Thomson Jr. <w@wltjr.com>
2024-11-15 10:53:56 -0400

    launch2/autonomy.launch.py: Add example parameters goal_poses & frame_id

--- launch2/autonomy.launch.py
+++ launch2/autonomy.launch.py
@@ -5,9 +5,12 @@ from launch_ros.actions import Node
 def generate_launch_description():
 
     autonomy_node = Node(package='autonomous',
-                               executable='autonomy_node',
-                               name='autonomy_node',
-                               parameters=[],
+                         executable='autonomy_node',
+                         name='autonomy_node',
+                         parameters=[
+                             {'goal_poses': [3.0, 3.0, 0.0]},
+                             {'frame_id': 'map'},
+                         ],
     )
 
     return LaunchDescription([
